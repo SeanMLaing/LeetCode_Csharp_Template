7d946efd406ad6b4a1367d5780f5d5a24676e78e
Sean <sean.laing@protonmail.com>
2023-11-18 16:37:46 -0700

    Accepted solution

--- LeetCodeTemplate/Program.cs
+++ LeetCodeTemplate/Program.cs
@@ -3,18 +3,49 @@ public class LeetCodeTemplate
 {
 
 
-    public static void Main()
+    public static void Main(string[] args)
     {
+        Console.WriteLine(Problem(args[0], args[1]));
+
         return;
-
-
     }
 
 
-    public static int Problem(int number)
+    public static string Problem(string word1, string word2)
     {
+        string returnstring = "";
 
-        return number;
+
+        if (word1.Length < 1 || word2.Length < 1)
+        {
+            return returnstring;
+        }
+
+        if (word1.Length > 100 || word2.Length > 100)
+        {
+            return returnstring;
+        }
+        
+
+        // make into arrays, 
+        char[] array1 = word1.ToCharArray();
+        char[] array2 = word2.ToCharArray();
+
+        int maxlength = array1.Length;
+        if (array2.Length > maxlength) 
+            maxlength = array2.Length;
+
+
+        for(int i = 0; i < maxlength; i++)
+        {
+            if (i < array1.Length)
+                returnstring += array1[i];
+
+            if (i < array2.Length)
+                returnstring += array2[i];
+        }
+
+        return returnstring.Replace(" ", "");
     }
 
 
--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -9,12 +9,34 @@ namespace UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod1(string[] args)
         {
-            int i = 0;
-            int result = LeetCodeTemplate.Problem(i);
+            string word1 = args[1], word2 = args[2];
 
-            Assert.AreEqual(i, result);
+            Assert.IsTrue(string.Equals(LeetCodeTemplate.Problem(word1, word2), "apbqcr"));
+
         }
+
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string word1 = "ab", word2 = "pqrs";
+
+            Assert.IsTrue(string.Equals(LeetCodeTemplate.Problem(word1, word2), "apbqrs"));
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string word1 = "abcd", word2 = "pq";
+
+            Assert.IsTrue(string.Equals(LeetCodeTemplate.Problem(word1, word2), "apbqcd"));
+
+        }
+
+
     }
 }
