310c83e7e10ec871a2fe1c8124b4940fc9964df5
Sean <sean.laing@protonmail.com>
2023-11-18 22:32:26 -0700

    Accepted answer

--- LeetCodeTemplate/Program.cs
+++ LeetCodeTemplate/Program.cs
@@ -5,19 +5,57 @@ public class LeetCodeTemplate
 
     public static void Main()
     {
+
+        int[] nums = { 27664, 47570, 27664, 27664, 27664, 27664, 27664, 27664, 27664, 27664 };
+
+        DateTime start = DateTime.Now;
+
+        Problem(nums);
+
+        Console.WriteLine( $"RunTime: {DateTime.Now - start}");
         return;
 
-
     }
 
+    // array sort 
 
-    public static int Problem(int number)
+    
+
+
+    public static int Problem(int[] nums)
     {
+        if (nums.Length > (5 * Math.Pow(10, 4)))
+        {
+            return 0;
+        }
 
-        return number;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > (5 * Math.Pow(10, 4)))
+                return 0;
+        }
+
+        int returnNum = 0;
+
+        Array.Sort(nums);
+
+        for(int i = nums.Length-1; i > 0; i--)
+        {
+
+            if (nums[i] != nums[i - 1])
+            {
+
+                returnNum += (nums.Length - i);
+            }
+        }
+
+
+
+
+        
+
+        return returnNum;
     }
-
-
 
 
 
--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -11,10 +11,67 @@ namespace UnitTests
         [TestMethod]
         public void TestMethod1()
         {
-            int i = 0;
+            int[] i = { 5, 1, 3 };
             int result = LeetCodeTemplate.Problem(i);
 
-            Assert.AreEqual(i, result);
+            Assert.AreEqual(3, result);
         }
-    }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] i = { 1, 1, 2, 2, 3 };
+            int result = LeetCodeTemplate.Problem(i);
+
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int[] i = { 1, 1, 1 };
+            int result = LeetCodeTemplate.Problem(i);
+
+            Assert.AreEqual(0, result);
+        }
+
+
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int[] i = { 27664, 47570, 27664, 27664, 27664, 27664, 27664, 27664, 27664, 27664};
+
+            int result = LeetCodeTemplate.Problem(i);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            int[] i = { 10832, 43690, (int)Math.Round((5 * Math.Pow(10, 4)) + 1)};
+            int result = LeetCodeTemplate.Problem(i);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            int[] i = { 49995, 49999, 49993, 49997, 49996, 50000, 49991, 49998, 49992, 49994 };
+            int result = LeetCodeTemplate.Problem(i);
+
+            Assert.AreEqual(45, result);
+        }
+        
+
+
+
+
+    } // EOC
+
+
+
+
 }
